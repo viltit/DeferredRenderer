012269e2b5358e9cf2b465c9b2e31e7f58521d04
Titus <Titus@VITI>
2016-09-22 21:15:03 +0200

    working on mesh class

--- vitiGL/Mesh.cpp
+++ vitiGL/Mesh.cpp
@@ -11,7 +11,7 @@
 namespace vitiGL {
 
 Mesh::Mesh(const std::string& filePath) {
-	//load object data with tinyObjLoader Library:
+	/* load object data with tinyObjLoader Library: */
 	tinyobj::attrib_t vertices;
 	std::vector<tinyobj::shape_t> shapes;
 	std::vector<tinyobj::material_t> materials;
@@ -20,11 +20,47 @@ Mesh::Mesh(const std::string& filePath) {
 	bool ret = tinyobj::LoadObj(&vertices, &shapes, &materials, &error, filePath.c_str());
 
 	if (!error.empty()) std::cout << error << std::endl;
-	if (!ret) vitiError(("<Mesh::Mesh>TinyObjLoader failed to load object " + filePath).c_str());
+	if (!ret) throw vitiError(("<Mesh::Mesh>TinyObjLoader failed to load object " + filePath).c_str());
 
+
+	/* convert the data into our own structures:
+		(we loose indexing on the way, but we have no support for it atm) */
+
+	std::vector<Vertex> myVertices(shapes[0].mesh.indices.size() / 3);
+	int j{ 0 };
+	for (int i = 0; i < shapes[0].mesh.indices.size(); i+=3) {
+		myVertices[j].pos.x = vertices.vertices[shapes[0].mesh.indices[i].vertex_index];
+		myVertices[j].pos.y = vertices.vertices[shapes[0].mesh.indices[i+1].vertex_index];
+		myVertices[j].pos.z = vertices.vertices[shapes[0].mesh.indices[i+2].vertex_index];
+
+		myVertices[j].uv.x = vertices.texcoords[shapes[0].mesh.indices[i].texcoord_index];
+		myVertices[j].uv.y = vertices.texcoords[shapes[0].mesh.indices[i+1].texcoord_index];
+
+		j++;
+	}
+	//check if normals are provided:
+	j = 0;
+	for (int i = 0; i < shapes[0].mesh.indices.size() && vertices.normals.size() != 0; i += 3) {
+		myVertices[j].normal.x = vertices.normals[shapes[0].mesh.indices[i].vertex_index];
+		myVertices[j].normal.y = vertices.normals[shapes[0].mesh.indices[i + 1].vertex_index];
+		myVertices[j].normal.z = vertices.normals[shapes[0].mesh.indices[i + 2].vertex_index];
+
+		j++;
+	}
+
+	//if no normals are provided, we need to calculate them:
+	if (vertices.normals.size() == 0) calcNormals(myVertices);
+
+	calcTangents(myVertices);
+	uploadVertices(myVertices);
+
+	//textures??? Model with several meshes??
 }
 
 
 Mesh::~Mesh() {
+}
+
+void Mesh::initVertices(std::vector<Vertex>& vertices) {
 }
 }
--- vitiGL/Mesh.hpp
+++ vitiGL/Mesh.hpp
@@ -3,16 +3,20 @@
 #pragma once
 
 #include <string>
+#include "Shape.hpp"
 
 namespace vitiGL {
 
-class Mesh {
+class Mesh : public Shape {
 public:
 	Mesh(const std::string& filePath);
 	~Mesh();
 
 private:
+	void initVertices();
 
+	// Inherited via Shape
+	virtual void initVertices(std::vector<Vertex>& vertices) override;
 };
 
 }
