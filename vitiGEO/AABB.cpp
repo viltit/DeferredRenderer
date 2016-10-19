71a6c65d79f85aaa28f69c9c18b9a056b868851d
viti <villiger.titus@bluewin.ch>
2016-10-19 15:45:00 +0200

    starting a vitiGEO namespace

---
+++ vitiGEO/AABB.cpp
@@ -0,0 +1,11 @@
+#include "AABB.hpp"
+
+namespace vitiGEO {
+
+AABB::AABB() {
+}
+
+
+AABB::~AABB() {
+}
+}
---
+++ vitiGEO/AABB.hpp
@@ -0,0 +1,15 @@
+/* Class to represent an axially aligned bounding box (AABB) */
+
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace vitiGEO {
+
+class AABB {
+public:
+	AABB();
+	~AABB();
+};
+}
+
