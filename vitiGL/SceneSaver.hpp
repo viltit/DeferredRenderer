31de0bc9d3a67fa59083e7b9c25c6dbd3d5d80ca
viltit <villiger.titus@bluewin.ch>
2017-03-09 17:07:14 +0100

    SceneSaver WIP

---
+++ vitiGL/SceneSaver.hpp
@@ -0,0 +1,25 @@
+#include <string>
+
+#include "tinyxml2.h"
+
+namespace vitiGL {
+
+class Scene;
+class SceneNode;
+
+class SceneSaver {
+public:
+	SceneSaver(Scene* scene, const std::string& fileName);
+	~SceneSaver();
+
+private:
+	bool saveScene(Scene* scene, const std::string& fileName);
+	bool processNode(SceneNode* sceneNode, tinyxml2::XMLNode* parent);
+	bool processObj(SceneNode* sceneNode, tinyxml2::XMLNode* parent);
+	bool processTransform(SceneNode* sceneNode, tinyxml2::XMLNode* parent);
+	bool processPhysics(SceneNode* sceneNode, tinyxml2::XMLNode* parent);
+
+
+	tinyxml2::XMLDocument _doc;
+};
+}
